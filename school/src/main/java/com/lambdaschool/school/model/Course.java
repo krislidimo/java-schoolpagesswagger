79c780ef626091ac04eaabf6a758090a09826579
Krisli Dimo <krislidimo@gmail.com>
2019-06-18 17:07:55 -0500

    added swagger documentation for models

--- school/src/main/java/com/lambdaschool/school/model/Course.java
+++ school/src/main/java/com/lambdaschool/school/model/Course.java
@@ -1,29 +1,36 @@
 package com.lambdaschool.school.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@ApiModel(value = "Course", description = "This model represents a course")
 @Entity
 @Table(name = "course")
 public class Course
 {
+    @ApiModelProperty(name = "courseid", value = "id of course", required = true, example = "1")
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
 //    @JsonView(View.CoursesOnly.class)
     private long courseid;
 
+    @ApiModelProperty(name = "coursename", value = "name of course", required = true, example = "Math")
 //    @JsonView(View.CoursesOnly.class)
     private String coursename;
 
+    @ApiModelProperty(name = "instructor", value = "instructor object", required = true)
     @ManyToOne
     @JoinColumn(name = "instructid")
     @JsonIgnoreProperties("courses")
 //    @JsonView(View.CoursesOnly.class)
     private Instructor instructor;
 
+    @ApiModelProperty(name = "students", value = "ArrayList of student Objects", required = true)
     @ManyToMany(mappedBy = "courses")
     @JsonIgnoreProperties("courses")
     private List<Student> students = new ArrayList<>();
